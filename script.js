aa7847e6bb19c9b65fdd39d2b9cc40a29103c0a5
radhikakumawat12 <kumawatradhika23@gmail.com>
2024-11-29 18:03:08 -0800

    first portfolio

---
+++ script.js
@@ -0,0 +1,50 @@
+/*let age = 22;
+if (age>=18)
+    console.log("u r eligible to vote!")
+else 
+console.log("u r not eligible to vote!")*/
+
+/*let marks=50;
+if(marks>=90 && marks <=100)
+    console.log("you lies in A grade category")
+else if(marks>=80 )
+    console.log("you lies in B grade category")
+else if(marks>=70 )
+    console.log("you lies in c grade category")
+else if(marks<70)
+    console.log("you lies in D grade category")
+else if(marks<30)
+    console.log("fail")
+else if(marks<=0 && marks>=100)
+    console.log("invalid marks entered")*/
+
+/*let grade = "A";
+switch (grade){
+    case "A":
+        console.log("Excellent")
+        break;
+     case "B":
+        console.log("very good")
+        break;  
+     case" C":
+        console.log("good")   
+        break;
+    case" D":
+        console.log("fair")   
+        break;
+     default:
+            console.log("default")
+}*/
+/*let i=4;
+while(i!=0)
+{
+    console.log(i)
+    i--
+}*/
+let i=5;
+do{
+    console.log("hello")
+    i--
+}while(i!=1)
+
+    
